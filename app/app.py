92df17eaaec3b50b5fdd20417b661c26c8712e92
Diogo Matos <alvesdiogo.mts@gmail.com>
2023-08-30 17:00:14 -0300

    feat: Get todos e create, get user por id

--- app/app.py
+++ app/app.py
@@ -31,6 +31,16 @@ def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
+# GET user por id
+@app.get("/users/{user_id}", tags=["users"], response_model=schemas.User)
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.read_user(db=db, user_id=user_id)
+    print(" ---- checar se usuario existe ---- ")
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Usuario nao encontrado")
+    return db_user
+
+
 # CREATE user
 @app.post("/users/", tags=["users"], response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
@@ -38,3 +48,18 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if new_user:
         raise HTTPException(status_code=400, detail="Email já em uso")
     return crud.create_user(db=db, user=user)
+
+
+# GET todos os ToDos
+@app.get("/todos/", tags=["todos"], response_model=list[schemas.Todo])
+def get_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.read_todos(db=db, skip=skip, limit=limit)
+
+
+# CREATE um ToDo
+@app.post("/users/{user_id}/todos", tags=["todos"], response_model=schemas.Todo)
+def create_todo_for_user(todo: schemas.TodoCreate, user_id: int, db: Session = Depends(get_db)):
+    user = crud.read_user(db=db, user_id=user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuario nao encontrado")
+    return crud.create_todo(db=db, todo=todo, user_id=user_id)
